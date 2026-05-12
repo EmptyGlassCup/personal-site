2670cac93cb75efb071c9abf66decea3fe5b0912
EmptyGlassCup <emptyglasscup@gmail.com>
2026-05-13 00:00:27 +1000

    Add dark mode

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,32 +2,32 @@ export default function Card() {
     return (
         <div id = "main-card" className = {`
         w-90 h-150 p-10 pt-20
-        border-solid border-[#0B3954] border-5 rounded-l-4xl
-        bg-white
+        border-solid border-[#0B3954] dark:border-[#3dccc7] border-5 rounded-l-4xl
+        bg-white dark:bg-[#0B3954]
         flex flex-col items-center justify-between
         relative z-2`}>
 
             <div id="main-top" className="flex flex-col items-center gap-4">
-                <div className = {`rounded-full border-solid border-[#0B3954] border-5 w-40 h-40`}></div>
-                <p className = "text-5xl text-center font-bold font-mono text-[#3dccc7]">Hyunjae Ha</p>
+                <div className = {`rounded-full border-solid border-[#0B3954] dark:border-[#3dccc7] border-5 w-40 h-40`}></div>
+                <p className = "text-5xl text-center font-bold font-mono text-[#3dccc7] dark:text-[#C4FFF9]">Hyunjae Ha</p>
             </div>
 
             <div id = "logos" className="flex justify-center items-center gap-3">
                 <a id="github" href="https://github.com/EmptyGlassCup" target="_blank">
-                    <svg className = "w-15 hover:w-16 transition-width duration-100 ease-in-out"fill="#0B3954" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"/></svg>
+                    <svg className = "w-15 hover:w-16 transition-width duration-100 ease-in-out fill-[#0B3954] dark:fill-[#3dccc7]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"/></svg>
                 </a>
                 <a id="linkedin" href="https://www.linkedin.com/in/hyunjaeha/" target="_blank">
-                    <svg className = "w-15 hover:w-16 transition-width duration-100 ease-in-out" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z" fill="#0B3954"/>
-                    <path d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z" fill="#0B3954"/>
-                    <path d="M11 19H12C12.5523 19 13 18.5523 13 18V13.5C13 12 16 11 16 13V18.0004C16 18.5527 16.4477 19 17 19H18C18.5523 19 19 18.5523 19 18V12C19 10 17.5 9 15.5 9C13.5 9 13 10.5 13 10.5V10C13 9.44771 12.5523 9 12 9H11C10.4477 9 10 9.44772 10 10V18C10 18.5523 10.4477 19 11 19Z" fill="#0B3954"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z" fill="#0B3954"/>
+                    <svg className = "w-15 hover:w-16 transition-width duration-100 ease-in-out fill-[#0B3954] dark:fill-[#3dccc7]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z"/>
+                    <path d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z"/>
+                    <path d="M11 19H12C12.5523 19 13 18.5523 13 18V13.5C13 12 16 11 16 13V18.0004C16 18.5527 16.4477 19 17 19H18C18.5523 19 19 18.5523 19 18V12C19 10 17.5 9 15.5 9C13.5 9 13 10.5 13 10.5V10C13 9.44771 12.5523 9 12 9H11C10.4477 9 10 9.44772 10 10V18C10 18.5523 10.4477 19 11 19Z"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z"/>
                     </svg>
                 </a>
             </div>
 
             <div id = "bottom-links">
-                <p className = "font-mono text-xs">Hello. Welcome to my Notebook! :)</p>
+                <p className = "font-mono text-xs dark:text-[#3dccc7]">Hello. Welcome to my Notebook! :)</p>
             </div>
         </div>
     );
--- src/components/info_card.tsx
+++ src/components/info_card.tsx
@@ -9,12 +9,13 @@ export default function Info_Card({ onAddClick, infoCard }: Info_CardProps){
     return(
         <div className = {`
         h-145 p-10
-        border-solid border-[#0B3954] border-5 rounded-r-4xl
-        bg-white
+        border-solid border-[#0B3954] border-5 rounded-r-4xl dark:border-[#3dccc7]
+        bg-white dark:bg-[#0B3954]
         absolute right-0 z-1
         flex flex-col justify-center items-center
         ${ infoCard ? "w-90" : "w-80"}
-        transition-width duration-400 ease-in-out`}>
+        transition-width duration-400 ease-in-out
+        dark:text-[#3dccc7]`}>
             <p>I really like Hatsune Miku</p>
             <p>Did you stumble upon this site?</p>
             <p>It's still under construction. Sorry!</p>
