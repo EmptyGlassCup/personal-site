d0838753232dff61f9b049abdb3d317e5dd86521
EmptyGlassCup <emptyglasscup@gmail.com>
2026-05-12 22:24:15 +1000

    Add SVG icons, adjust colour

--- src/components/info_card.tsx
+++ src/components/info_card.tsx
@@ -8,14 +8,16 @@ interface Info_CardProps {
 export default function Info_Card({ onAddClick, infoCard }: Info_CardProps){
     return(
         <div className = {`
-        h-150 p-10
-        border-solid border-black border-5
+        h-145 p-10
+        border-solid border-[#0B3954] border-5 rounded-r-4xl
         bg-white
-        absolute right-0 z-1
-        flex justify-center items-center
+        absolute right-0 top-2 z-1
+        flex flex-col justify-center items-center
         ${ infoCard ? "w-90" : "w-80"}
         transition-width duration-400 ease-in-out`}>
-            <p>Boo!</p>
+            <p>I really like Hatsune Miku</p>
+            <p>Did you stumble upon this site?</p>
+            <p>It's still under construction. Sorry!</p>
             <Pull_Tab onAddClick={ onAddClick } />
         </div>
     );
--- src/components/pull_tab.tsx
+++ src/components/pull_tab.tsx
@@ -5,11 +5,20 @@ interface Pull_TabProps{
 export default function Pull_Tab({ onAddClick }: Pull_TabProps){
     return(
         <div onClick={ onAddClick } className={`
-        w-20 h-20
-        border-black border-5
+        w-25 h-25
+        border-[#0B3954] border-5
         bg-white
+        hover:bg-[#9CEAEF]
+        duration-100
         rounded-r-3xl
-        absolute -right-20 top-60`}>
+        absolute -right-25 top-60
+        flex flex-col justify-center items-center`}>
+
+            <svg className = "w-7"viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="#000000"/>
+                <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="#0B3954"/>
+            </svg>
+            <p className = "font-mono text-xs text-[#0B3954]">about me</p>
         </div>
     )
 }
