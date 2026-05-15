c8ee38f84d13f9dde446f9424eb32875515e70e4
EmptyGlassCup <emptyglasscup@gmail.com>
2026-05-15 21:56:21 +1000

    Remove useless translate class

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -11,7 +11,11 @@ export default function Wrapper(){
     }
 
     return (
-        <div id = "wrapper" className = {`flex items-center relative ${ infoCard ? "w-179" : "w-90"} transition-width duration-400 ease-in-out`}>
+        <div id = "wrapper" className = {`
+        flex items-center
+        relative
+        ${ infoCard ? "w-179" : "w-90"}
+        duration-400 ease-in-out`}>
             <Card />
             <Info_Card onAddClick = { addCard } infoCard={ infoCard } />
         </div>
